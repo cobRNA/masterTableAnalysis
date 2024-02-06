a809ebb2aaeda89395242394ce4fbae29d1fc7bf
Tomasz Mądry <tke.madry@gmail.com>
2024-02-06 20:16:05 +0100

    minor changes

--- Bash_scripts/TM_README.sh
+++ Bash_scripts/TM_README.sh
@@ -13,28 +13,42 @@ do
     while read end dist
     do
         echo $end
-        cat $file | ./Utils/extractTranscriptEndsFromBed12.pl $end | ./Utils/sortbed | ./Utils/bedtools merge -s -d $dist -c 4 -o collapse -i stdin | awk '{print $1"\t"$2"\t"$3"\t"$5"\t0\t"$4}' > ./Data/Processed/$lab.$end.bed
+        cat $file | ./Utils/extractTranscriptEndsFromBed12.pl $end | ./Utils/sortbed  > ./Data/Processed/Extracted_ends/$lab.$end.bed
     done < ./Data/Others/ends.dist.tsv
 done
+
+# bez tego:
+# | ./Utils/bedtools merge -s -d $dist -c 4 -o collapse -i stdin
+# | awk '{print $1"\t"$2"\t"$3"\t"$5"\t0\t"$4}'
 
 
 # -------------------
 # polyA
 # -------------------
 
-for file in `ls ./Data/Catalogues/*.hg38.bed12`
+while read sampl
 do
-    echo $file
-    lab=`basename $file| awk -F "." '{print $1}'`
+    name=`echo $sampl`
+    echo $name
     while read end dist
     do
-        echo $end
-        cat ./Data/Processed/$lab.$end.bed | ./Utils/sortbed | ./Utils/bedtools slop -s -l 5 -r -5 -i stdin -g ./Data/Others/chromInfo_hg38.txt | ./Utils/bedtools intersect -u -s -a stdin -b ./Data/Others/hg38.polyAsignals.bed > ./Data/Processed/$lab.$end.bed.vspolyAsignals.bedtsv
+       cat ./Data/Processed/Extracted_ends/$name.$end.bed | ./Utils/sortbed | ./Utils/jlagarde/bin.bkp/bedtools2/bin/bedtools slop -s -l 5 -r 5 -i stdin -g ./Data/Others/chromInfo_hg38.txt | ./Utils/jlagarde/bin.bkp/bedtools2/bin/bedtools intersect -u -s -a stdin -b ./Data/Others/hg38.polyAsignals.bed > ./Data/Processed/PolyA/$name.$end.bed.vspolyAsignals.bedtsv
     done < ./Data/Others/ends.dist.tsv
-done
+done < ./Data/Others/samples.tsv
 
 
 
+# -----------------------
+# CAGE FANTOM pahse 1+2
+# -----------------------
 
- 
+while read sampl
+do
+    name=`echo $sampl`
+    echo $name
+    while read end dist
+    do
+        cat ./Data/Processed/Extracted_ends/$name.$end.bed | ./Utils/sortbed | ./Utils/jlagarde/bin.bkp/bedtools2/bin/bedtools slop -s -l 50 -r 50 -i stdin -g ./Data/Others/chromInfo_hg38.txt | ./Utils/jlagarde/bin.bkp/bedtools2/bin/bedtools intersect -u -s -a stdin -b ./Data/Others/hg38_fair+new_CAGE_peaks_phase1and2.bed > ./Data/Processed/Cage/$name.$end.bed.vsCage.fantom.bedtsv
+    done < ./Data/Others/ends.dist.tsv
+done < ./Data/Others/samples.tsv
 
