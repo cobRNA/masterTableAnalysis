07139632cd5edcf0f4181f0c56527fa749e4958a
Tomasz Mądry <tke.madry@gmail.com>
2024-02-19 19:47:08 +0100

    initial commit

---
+++ Python_scripts/mt_decode_sample.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+ 1. Read transcript from stdin;
+ 2. Find transcript_id and sample metadata code/s;
+ 3. Split samples into different lines;
+ 4. Substitude sample codes with sample metadata from Hv3_sampleCoding file;
+ 5. Return transcript_id \t full sample metadata line into stdout.
+"""
+
+
+import sys
+
+# Create dictionary to store metadata
+meta_data = {}
+# Read metadata from file
+PATH = "/home/tomasz/Desktop/mT/Hv3_sampleCoding"
+with open(PATH, "r", encoding="utf-8") as file:
+    lines = file.readlines()
+    for l in lines:
+        line = l.split()
+        meta_data[line[0]] = line[1]
+
+# handle the header line
+HEADER = sys.stdin.readline().replace("sample", "tissue")
+sys.stdout.write(HEADER)
+
+for l in sys.stdin:
+    line = l.split()
+    codes = line[6].strip('";').split(",")
+    for code in codes:
+        # create output string
+        output_line = "\t".join(line[:-1]) + "\t" + meta_data[code] + "\n"
+        sys.stdout.write(output_line)  # return the line to stdout
---
+++ Python_scripts/mt_split_targets_with_samples.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+ Read transcripts from mT, split each target into new line and write tsv formatted output
+ lines to stdout. Written for bash pipeline.
+"""
+
+import sys
+
+# Write header to stdout
+HEADER = ["chr", "type", "strand", "gene_id", "transcript_id", "target", "sample"]
+
+sys.stdout.write("\t".join(HEADER) + "\n")
+
+# Create tsv line
+for l in sys.stdin:
+    line = l.replace('""', "NA").replace('"', "").replace(";", "").split()
+    targets = line[13].split(",")
+    samples = line[25].split(",")
+    for target in targets:
+        for sample in samples:
+            output_line = [line[0], line[2], line[6], line[9], line[11], target, sample]
+            sys.stdout.write("\t".join(output_line) + "\n")
