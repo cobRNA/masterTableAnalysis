d421c89d697743c180bb783d7be5d70a402f4167
Tomasz Mądry <tke.madry@gmail.com>
2024-02-20 19:06:38 +0100

    endSupport column added

--- Python_scripts/mt_split_targets_with_samples.py
+++ Python_scripts/mt_split_targets_with_samples.py
@@ -7,7 +7,16 @@
 import sys
 
 # Write header to stdout
-HEADER = ["chr", "type", "strand", "gene_id", "transcript_id", "target", "sample"]
+HEADER = [
+    "chr",
+    "type",
+    "strand",
+    "gene_id",
+    "transcript_id",
+    "target",
+    "endSupport",
+    "sample",
+]
 
 sys.stdout.write("\t".join(HEADER) + "\n")
 
@@ -18,5 +27,14 @@ for l in sys.stdin:
     samples = line[25].split(",")
     for target in targets:
         for sample in samples:
-            output_line = [line[0], line[2], line[6], line[9], line[11], target, sample]
+            output_line = [
+                line[0],
+                line[2],
+                line[6],
+                line[9],
+                line[11],
+                target,
+                line[15],
+                sample,
+            ]
             sys.stdout.write("\t".join(output_line) + "\n")
