dc7b411aad2bdc09426a68eff6fa3aa86ff3e5eb
Tomasz Mądry <tke.madry@gmail.com>
2024-03-26 19:52:51 +0100

    moved tmerge content to tmerging_catalogues.sh

--- Bash_scripts/get_anchored_transcript_ends.sh
+++ Bash_scripts/get_anchored_transcript_ends.sh
@@ -15,54 +15,33 @@ for catalogue in ./Data/Catalogues/*bed12
         # Convert catalogue to required gff format
         cat $catalogue | ./Utils/bed12togff > ./Data/Processed/Catalogues_gff/$filename.gff
 
-        # Create clusters for each end
         cat_name=$(echo $filename | awk -F'.' '{print $1}')
-        ## 5' (cage)
-        bedtools merge -c 4 -o collapse -s -d 5 -i ./Data/Processed/Cage/$cat_name.5.bed.vsCage.fantom.bedtsv > ./Data/Processed/Clusters/${cat_name}_5p_clusters.bed
-        ## 3' (polyA)
-        bedtools merge -c 4 -o collapse -s -d 5 -i ./Data/Processed/PolyA/$cat_name.3.bed.vspolyAsignals.bedtsv > ./Data/Processed/Clusters/${cat_name}_3p_clusters.bed
+        
+        # # Create clusters for each end
+        # ## 5' (cage)
+        # bedtools merge -c 4 -o collapse -s -d 5 -i ./Data/Processed/Cage/$cat_name.5.bed.vsCage.fantom.bedtsv > ./Data/Processed/Clusters/${cat_name}_5p_clusters.bed
+        # ## 3' (polyA)
+        # bedtools merge -c 4 -o collapse -s -d 5 -i ./Data/Processed/PolyA/$cat_name.3.bed.vspolyAsignals.bedtsv > ./Data/Processed/Clusters/${cat_name}_3p_clusters.bed
 
-        # Run anchoring
-        ./Utils/anchorTranscriptsEnds.pl ./Data/Processed/Catalogues_gff/$filename.gff ./Data/Processed/Cage/$cat_name.tx.cage ./Data/Processed/PolyA/$cat_name.tx.polyA ./Data/Processed/Clusters/${cat_name}_5p_clusters.bed ./Data/Processed/Clusters/${cat_name}_3p_clusters.bed > ./Data/Processed/Anchored_catalogues/anchored_${filename}.gff                     
+        # Sort before anchoring
+        cat ./Data/Processed/Catalogues_gff/$filename.gff | sort -k1,1 -k4,4n -k5,5n "$@" > ./Data/Processed/Catalogues_gff/${filename}_sorted.gff
+
+        # Run anchoring on sorted content
+        ./Utils/anchorTranscriptsEnds.pl ./Data/Processed/Catalogues_gff/${filename}_sorted.gff ./Data/Processed/Cage/$cat_name.tx.cage ./Data/Processed/PolyA/$cat_name.tx.polyA ./Data/Processed/Clusters/${cat_name}_5p_clusters.bed ./Data/Processed/Clusters/${cat_name}_3p_clusters.bed > ./Data/Processed/Anchored_catalogues/anchored_${filename}.gff                     
+
+
+        # # Run anchoring
+        # ./Utils/anchorTranscriptsEnds.pl ./Data/Processed/Catalogues_gff/$filename.gff ./Data/Processed/Cage/$cat_name.tx.cage ./Data/Processed/PolyA/$cat_name.tx.polyA ./Data/Processed/Clusters/${cat_name}_5p_clusters.bed ./Data/Processed/Clusters/${cat_name}_3p_clusters.bed > ./Data/Processed/Anchored_catalogues/anchored_${filename}.gff                     
         done;
 
 
 
 
-
-
+# Usuwając transkrypty ENST z fantomcata znikają konflikty z cls
+# cat FANTOM_CAT.lv4_stringent.only_lncRNA.gtf | grep -v 'ENST' 
 
 ###############
 # Merge catalogues with anchored FL trancript models.
 # Remove fake exons after merging.
 ###############
 
-cat ./Data/Processed/Anchored_catalogues/*.gff | sort -k1,1 -k4,4n -k5,5n | ./Utils/tmerge - > ./Data/Processed/Merged_anchored_catalogues/lncrna.anchor.merged.gtf
-
-# | grep -v 'fakeExon "yes";'
-
-
-
-
-
-# for catalogue in ./Data/Catalogues/*bed12
-#     do
-#         # Get filename
-#         filename=$(basename -- "$catalogue")
-#         extension="${filename##*.}"
-#         filename="${filename%.*}"
-#         echo $filename
-
-#         # Convert catalogue to required gff format
-#         cat $catalogue | ./Utils/bed12togff > ./Data/Processed/Catalogues_gff/$filename.gff
-
-#         # Create clusters for each end
-#         cat_name=$(echo $filename | awk -F'.' '{print $1}')
-#         ## 5' (cage)
-#         bedtools merge -d 5 -s -c 4,6 -o distinct -i ./Data/Processed/Cage/$cat_name.5.bed.vsCage.fantom.bedtsv > ./Data/Processed/Clusters/${cat_name}_5p_clusters.bed
-#         ## 3' (polyA)
-#         bedtools merge -d 5 -s -c 4,6 -o distinct -i ./Data/Processed/PolyA/$cat_name.3.bed.vspolyAsignals.bedtsv > ./Data/Processed/Clusters/${cat_name}_3p_clusters.bed
-
-#         # Run anchoring
-#         ./Utils/anchorTranscriptsEnds.pl ./Data/Processed/Catalogues_gff/$filename.gff ./Data/Processed/Cage/$cat_name.tx.cage ./Data/Processed/PolyA/$cat_name.tx.polyA ./Data/Processed/Clusters/${cat_name}_5p_clusters.bed ./Data/Processed/Clusters/${cat_name}_3p_clusters.bed > ./Data/Processed/Anchored_catalogues/anchored_${filename}.gff                     
-#         done;
